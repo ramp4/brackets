46d7d5466c57b439d5ce371e8d64c78ec61cc3da
ramp4 <rampatv13@gmail.com>
2019-03-23 23:03:43 +0300

    feat: add function which checks for correct brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let lengthConfig = bracketsConfig.length;
+  let pair = {};
+  for (i = 0; i < lengthConfig; i++) {
+    pair[bracketsConfig[i][0]] = bracketsConfig[i][1];
+   
+  }
+  let stack = [];
+  let lengthStr = str.length; 
+  for (i = 0; i < lengthStr; i++) {
+    let char = str.charAt(i);
+        for (j = 0; j < lengthConfig; j++) {
+          if (char == bracketsConfig[j][0]) { 
+            stack.push(pair[char]); 
+          }
+          if (char == bracketsConfig[j][1]) {
+            if (0 == stack.length)
+              return false;
+            if (stack[stack.length - 1] != char ) 
+              return false;  
+          stack.pop();            
+          };
+        };
+    
+  };
+  if (stack.length) 
+      return false;
+  return true;
 }
